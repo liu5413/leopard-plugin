0db65db69c0d4fcdb634e7d9f3c69b8ad7fa8360
GitHub Action <action@github.com>
2026-04-22 08:24:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/liu5413/leopardplugin/MyBundle.kt
+++ src/main/kotlin/com/github/liu5413/leopardplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.liu5413.leopardplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/liu5413/leopardplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/liu5413/leopardplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.liu5413.leopardplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liu5413.leopardplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
